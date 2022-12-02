432ae67bf7fbabd31ca053be13a9fa76b61079d1
drrnsyk <darren.syk@gmail.com>
2022-12-03 00:48:53 +0800

    added home page, background video

---
+++ src/main/java/vttp2022/project/controller/TinywhoopController.java
@@ -0,0 +1,11 @@
+package vttp2022.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/race-course")
+public class TinywhoopController {
+    
+}
+
