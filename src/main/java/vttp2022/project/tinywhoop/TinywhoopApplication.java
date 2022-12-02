d7d15c3a2b73f6e90423b0b0b73cb43463699fc8
drrnsyk <darren.syk@gmail.com>
2022-12-02 13:55:39 +0800

    initial commit

---
+++ src/main/java/vttp2022/project/tinywhoop/TinywhoopApplication.java
@@ -0,0 +1,13 @@
+package vttp2022.project.tinywhoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinywhoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinywhoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/project/tinywhoop/TinywhoopApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.project.tinywhoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinywhoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
