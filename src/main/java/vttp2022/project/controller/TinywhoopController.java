f0ab75c39e843233dd2cfe32cc1c214af16386d3
drrnsyk <darren.syk@gmail.com>
2022-12-05 02:34:35 +0800

    added TinyWhoopController

--- src/main/java/vttp2022/project/controller/TinywhoopController.java
+++ src/main/java/vttp2022/project/controller/TinywhoopController.java
@@ -1,11 +1,21 @@
 package vttp2022.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/race-course")
-public class TinywhoopController {
-    
+@RequestMapping("/racecourse")
+public class TinyWhoopController {
+
+    @GetMapping
+    public String getAllRaceCourses(@RequestParam String userName, Model model) {
+        System.out.println(userName);
+        model.addAttribute("userName", userName);
+        return "racecourse";
+    }
+
 }
 
