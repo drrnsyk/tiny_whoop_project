e8e6ae53ebff61e7d0740affe57dc71b6e3dd809
drrnsyk <darren.syk@gmail.com>
2022-12-05 02:33:38 +0800

    updated RaceCourse model

--- src/main/java/vttp2022/project/model/RaceCourse.java
+++ src/main/java/vttp2022/project/model/RaceCourse.java
@@ -1,13 +1,15 @@
 package vttp2022.project.model;
 
 import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 
 public class RaceCourse {
     private Integer raceId;
     private String raceName;
     private Integer laps;
     private DateTime closingDate;
-    
+
     public Integer getRaceId() {
         return raceId;
     }
@@ -33,6 +35,16 @@ public class RaceCourse {
         this.closingDate = closingDate;
     }
 
+    public static RaceCourse create(SqlRowSet rs) {
+        RaceCourse rc = new RaceCourse();
+        rc.setRaceId(rs.getInt("race_id"));
+        rc.setRaceName(rs.getString("race_name"));
+        rc.setLaps(rs.getInt("laps"));
+        rc.setClosingDate(new DateTime(
+                DateTimeFormat.forPattern("dd/MM/yyyy")
+                        .parseDateTime(rs.getString("closing_date"))));
+        return rc;        
+    }
     
 
 }
