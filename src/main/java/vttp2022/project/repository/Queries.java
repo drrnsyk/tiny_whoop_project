7033fa14093f9a9470575700b141a53c248e9274
drrnsyk <darren.syk@gmail.com>
2022-12-06 01:07:57 +0800

    added sql insert race course query

--- src/main/java/vttp2022/project/repository/Queries.java
+++ src/main/java/vttp2022/project/repository/Queries.java
@@ -4,4 +4,6 @@ public class Queries {
     public static final String SQL_SELECT_ALL_RACE_COURSES = """
             SELECT * FROM race_courses
             """;
+
+    public static String SQL_INSERT_RACE_COURSE = "INSERT INTO race_courses(race_name, laps, closing_date) VALUES (?, ?, SYSDATE())";
 }
